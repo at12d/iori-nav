fb808af24b5fef8e7133ab335247f37c3ae3a093
at12d <at123@at6.kdns.fr>
2026-08-10 02:53:35 +0800

    Add files via upload

--- public/js/home-search.js
+++ public/js/home-search.js
@@ -70,6 +70,7 @@
       let placeholder = '搜索书签...';
       switch (engine) {
         case 'google': placeholder = 'Google 搜索...'; break;
+        case 'bing': placeholder = 'Bing 搜索...'; break;
         case 'baidu': placeholder = '百度搜索...'; break;
         case 'github': placeholder = 'Github 搜索...'; break;
       }
@@ -117,10 +118,6 @@
 
     if (engineOptions.length > 0) {
       currentSearchEngine = localStorage.getItem('search_engine') || 'local';
-      if (currentSearchEngine === 'bing') {
-        currentSearchEngine = 'github';
-        localStorage.setItem('search_engine', currentSearchEngine);
-      }
       updateSearchEngineUI(currentSearchEngine);
     } else {
       localStorage.removeItem('search_engine');
@@ -159,6 +156,7 @@
             let url = '';
             switch (currentSearchEngine) {
               case 'google': url = `https://www.google.com/search?q=${encodeURIComponent(query)}`; break;
+              case 'bing': url = `https://www.bing.com/search?q=${encodeURIComponent(query)}`; break;
               case 'baidu': url = `https://www.baidu.com/s?wd=${encodeURIComponent(query)}`; break;
               case 'github': url = `https://github.com/search?q=${encodeURIComponent(query)}`; break;
             }
